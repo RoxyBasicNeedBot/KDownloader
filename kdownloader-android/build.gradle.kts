cd3e109a159e4589bec83797a599caef3654fee4
RoxyBasicNeedBot <235558257+RoxyBasicNeedBot@users.noreply.github.com>
2026-06-18 10:24:58 -0700

    Fix compilerOptions configuration block location in Android modules

--- kdownloader-android/build.gradle.kts
+++ kdownloader-android/build.gradle.kts
@@ -17,7 +17,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
